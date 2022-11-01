907ec2ebfadbeb25380b211f9e2f78370188ee0b
SaeedQatanani <saeedqatananil@gmail.com>
2022-11-01 10:01:18 +0200

    edited

--- firstproject/src/main/java/com/saeed/firstproject/FirstprojectApplication.java
+++ firstproject/src/main/java/com/saeed/firstproject/FirstprojectApplication.java
@@ -2,23 +2,13 @@ package com.saeed.firstproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 
-@RestController
 public class FirstprojectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstprojectApplication.class, args);
 	}
-	@RequestMapping("/")
-	public String hello() {
-		return "Hello world";
-	}
-	@RequestMapping("/new_route")
-	public String goodbye() {
-		return "Bye Bye world";
-	}
+
 }
